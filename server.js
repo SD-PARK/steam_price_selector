b6c383c0c89dc02371f37caad27472bda3d19d71
SD-PARK <tkdeh129@naver.com>
2023-05-02 19:11:53 +0900

    feat: 서버 초기 설정

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const config = require('./config');
+
+const PORT = config.PORT;
+
+// 외부 도메인 접근 관련
+// const cors = require('cors');
+// let corsOptions = {
+//     origin: '*',
+//     credential: true
+// }
+// app.use(cors(corsOptions));
+
+const router = require('./api/testAPI');
+app.use('/', router);
+
+server.listen(PORT, () => { console.log('Server listen on PORT', PORT); });
