1814c14fda5e1b694caa6e2e60acb4e331215675
SD-PARK <tkdeh129@naver.com>
2023-05-03 18:14:31 +0900

    fix: GPU 이름으로 검색

--- api/testAPI.js
+++ api/testAPI.js
@@ -4,14 +4,14 @@ const gpu = require('../data/gpu');
 
 router.get('/', (req, res) => {
     // res.send('This Message from the Router.');
-    data = gpu.find(isData);
-    res.send(data);
+    let name = 'GeForce RTX 306';
+    data = gpu.find((element) => {
+        if(element.name === name) {
+            return true;
+        }
+    });
+    res.send(data?.value ?? '0');
+    console.log(data?.value ?? 0);
 });
-
-function isData(element) {
-    if(element.name === 'GeForce RTX 3060') {
-        return true;
-    }
-}
 
 module.exports = router;
