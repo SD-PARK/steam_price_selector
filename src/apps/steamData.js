1db775ada5d607c55994cb80046237a0f34e9bc0
SD-PARK <tkdeh129@naver.com>
2023-05-06 13:41:45 +0900

    refactor: 변수 이름 변경 appSystemRequirements -> appInfos

--- src/apps/steamData.js
+++ src/apps/steamData.js
@@ -5,19 +5,18 @@ const useJSON = require('./useJSON');
 
 const batchSize = 200; // 한 번에 처리할 게임 개수
 const interval = (5 * 60 * 1000) + 1000; // API 호출 간격 (밀리초 단위)
-let batchs; // setTimeout 예약용 변수
 let appIDs = []; // 모든 게임의 app ID
 let appNames = []; // 모든 게임의 이름
-let appSystemRequirements = []; // 모든 게임의 시스템 요구사항
+let appInfos = []; // 새로 저장할 게임의 시스템 요구사항
 
 /** 신규 게임 데이터를 업데이트합니다. */
 const updateGameData = async () => {
     await getAppList();
-    appSystemRequirements = await useJSON.readJSON('gameData.json');
+    appInfos = await useJSON.readJSON('gameData.json');
     const omissions = await omissionCheck();
     for (const id of omissions) {
         // 오류 검사
-        if (appSystemRequirements.find((app) => {app.id === id})) {
+        if (appInfos.find((app) => {app.id === id})) {
             console.log(`\n\n\n\n\n${id} is Duplicate value!!!!!\n\n\n\n\n`);
             continue;
         }
@@ -29,7 +28,7 @@ const updateGameData = async () => {
             const minimumRequirementsObject = extractData(minimum);
             const recommendedRequirementsObject = extractData(recommended);
 
-            appSystemRequirements.push({
+            appInfos.push({
                 name: appNames[appIDs.indexOf(id)],
                 id: id,
                 is_free: details.is_free,
@@ -47,7 +46,7 @@ const updateGameData = async () => {
         })
         .catch(error => {
             if (error.message === 'No app found') {
-                appSystemRequirements.push({
+                appInfos.push({
                     name: appNames[appIDs.indexOf(id)],
                     id: id,
                     requirements: {},
@@ -58,7 +57,7 @@ const updateGameData = async () => {
             }
         });
     }
-    await useJSON.writeJSON(appSystemRequirements, 'gameData.json');
+    await useJSON.writeJSON(appInfos, 'gameData.json');
 }
 
 /** 게임 데이터를 전체 게임 리스트와 비교해 끊긴 부분부터 이어서 불러와 저장 */
@@ -67,11 +66,12 @@ const writeGameDataContinue = async () => {
     appIDs = games.map(game => game.appid);
     appNames = games.map(game => game.name);
 
-    appSystemRequirements = await useJSON.readJSON('gameData.json');
+    appInfos = await useJSON.readJSON('gameData.json');
     
-    const startIndex = appSystemRequirements.length;
+    const startIndex = appInfos.length;
     if (startIndex < appIDs.length) {
-        await getNextBatch(startIndex).then(async () => { await useJSON.writeJSON(appSystemRequirements, 'gameData.json'); });
+        console.log('Continue indexing from', startIndex);
+        await getNextBatch(startIndex).then(async () => { await useJSON.writeJSON(appInfos, 'gameData.json'); });
     } else {
         console.log('GameData Upload Completed!');
     }
@@ -104,22 +104,22 @@ async function getAppList() {
     });
 }
 
-/** 게임의 상세 데이터를 불러와 gameData.json에 저장합니다. !!호출 전 반드시 appIDs와 appNames를 초기화할 것!! */
+/** 게임의 세부 데이터를 불러와 gameData.json에 저장합니다. !!호출 전 반드시 appIDs와 appNames를 초기화할 것!! */
 async function getNextBatch(startIndex) {
     const endIndex = Math.min(startIndex + batchSize, appIDs.length);
     const batchIDs = appIDs.slice(startIndex, endIndex);
     
     // 다음 호출을 예약, 호출을 마치면 JSON 파일에 저장
     if (endIndex < appIDs.length) {
-        batchs = setTimeout(() => getNextBatch(endIndex).then(async () => { await useJSON.writeJSON(appSystemRequirements, 'gameData.json'); }), interval);
+        setTimeout(() => getNextBatch(endIndex).then(async () => { await useJSON.writeJSON(appInfos, 'gameData.json'); }), interval);
     }
 
     // API 호출
     // 'Promise.all'을 사용할 때 'map' 함수를 사용해 배열을 순회하며 각 요소를 비동기적으로 처리할 수 있지만, 내부적으로는 병렬적으로 처리되기 때문에 결과의 순서가 보장되지 않음.
-    // 따라서 전체 게임 리스트(games.json)과 상세 데이터(gameData.json)의 배열 순서를 보장하기 위해 for of 사용.
+    // 따라서 전체 게임 리스트(games.json)과 세부 데이터(gameData.json)의 배열 순서를 보장하기 위해 for of 사용.
     for (const id of batchIDs) {
         // 오류 검사
-        if (appSystemRequirements.find((app) => {app.id === id}))
+        if (appInfos.find((app) => {app.id === id}))
             throw `\n\n\n\n\n${id} is Duplicate value!!!!!\n\n\n\n\n`;
 
         await steam.getGameDetails(id)
@@ -129,7 +129,7 @@ async function getNextBatch(startIndex) {
             const minimumRequirementsObject = extractData(minimum);
             const recommendedRequirementsObject = extractData(recommended);
 
-            appSystemRequirements.push({
+            appInfos.push({
                 name: appNames[appIDs.indexOf(id)],
                 id: id,
                 is_free: details.is_free,
@@ -147,7 +147,7 @@ async function getNextBatch(startIndex) {
         })
         .catch(error => {
             if (error.message === 'No app found') {
-                appSystemRequirements.push({
+                appInfos.push({
                     name: appNames[appIDs.indexOf(id)],
                     id: id,
                     requirements: {},
