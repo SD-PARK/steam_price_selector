5985718da1643dd2c6c6950004e7d6b92cda452a
SD-PARK <tkdeh129@naver.com>
2023-05-14 16:44:40 +0900

    test: 데이터 파싱 테스트

--- src/routes/testAPI.js
+++ src/routes/testAPI.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const useJSON = require('../apps/useJSON');
 // require('../apps/gpuUpdate')(); // GPU Data 갱신
-require('../apps/steamData').checkSingleApp(10); // Steam Data 갱신
-// require('../apps/steamData').checkSingleApp(271590); // Steam Data 갱신
-// require('../apps/steamData').checkSingleApp(990080); // Steam Data 갱신
-// require('../apps/steamData').checkSingleApp(1150530); // Steam Data 갱신
-// require('../apps/steamData').checkSingleApp(1415280); // Steam Data 갱신
+// require('../apps/steamData').checkSingleApp(10); // Steam Data 갱신 // Counter-Strike
+// require('../apps/steamData').checkSingleApp(271590); // Steam Data 갱신 // Grand Theft Auto 5
+require('../apps/steamData').checkSingleApp(990080); // Steam Data 갱신 // Hogwarts Legacy
+// require('../apps/steamData').checkSingleApp(2369390); // Steam Data 갱신 // Farcry 6
+// require('../apps/steamData').checkSingleApp(1150530); // Steam Data 갱신 // Wizard with a Gun
+// require('../apps/steamData').checkSingleApp(1415280); // Steam Data 갱신 // Faerie Afterlight
 // require('../apps/steamData').findOmission();
 
 router.get('/', async (req, res) => {
