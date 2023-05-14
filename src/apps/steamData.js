480dc8ceddb944629fbdba620ce2ba5cae07c403
SD-PARK <tkdeh129@naver.com>
2023-05-14 15:21:27 +0900

    refactor: steam api 사용, 함수 이름 변경 및 주석문 추가

--- src/apps/steamData.js
+++ src/apps/steamData.js
@@ -1,118 +1,90 @@
-const { chromium } = require('playwright');
 const SteamAPI = require('steamapi');
 const API_KEY = require('../../config/secret').STEAM_API_KEY;
 const steam = new SteamAPI(API_KEY);
 const useJSON = require('./useJSON');
 
-const batchSize = 20; // 한 번에 처리할 게임 개수
-const interval = 0.3 * 60 * 1000; // 크롤링 호출 간격 (밀리초 단위)
+const batchSize = 200; // 한 번에 처리할 게임 개수
+const interval = (5 * 60 * 1000) + 1000; // API 호출 간격 (밀리초 단위)
 let appIDs = []; // 모든 게임의 app ID
 let appNames = []; // 모든 게임의 이름
 let appInfos = []; // 새로 저장할 게임의 시스템 요구사항
 
-/** 신규 게임 데이터를 업데이트합니다. */
-const updateGameData = async () => {
-    // await getAppList();
-    // appInfos = await useJSON.readJSON('gameData.json');
-    // const omissions = await omissionCheck();
-    // await useJSON.writeJSON(appInfos, 'gameData.json');
+/**
+ * 게임 데이터를 업데이트합니다. 게임 목록을 가져와 JSON 파일에 저장한 후, 작성되지 않은 게임들의 정보를 API를 통해 가져와 게임 데이터를 추가합니다.
+ * 추가된 게임 데이터는 JSON 파일에 저장됩니다.
+ */
+const updateGameDatabase = async () => {
+    await fetchAppList();
+    appInfos = await useJSON.readJSON('gameData.json');
+    const omissions = await findOmission();
+    for (const id of omissions) {
+        if (appInfos.find((app) => {app.id === id})) {
+            console.log(`\n\n\n\n\n${id} is Duplicate value!!!!!\n\n\n\n\n`);
+            continue;
+        }
+
+        fetchGameDetails(id);
+    }
+    await useJSON.writeJSON(appInfos, 'gameData.json');
 }
 
-/** 세부 데이터가 업로드되지 않은 게임 크롤링 */
-const writeGameDataContinue = async () => {
-    const games = omissionCheck();
-    appIDs = (await games).map(game => game.appid);
-    appNames = (await games).map(game => game.name);
-
+/**
+ * 게임 데이터 작성 작업을 계속 진행합니다. JSON 파일에서 게임 목록을 읽어와 필요한 정보를 추출하고, 지금까지 작성된 게임 데이터를 불러옵니다.
+ * 이어서 작성 작업을 진행하며, 아직 작성되지 않은 게임들의 정보를 API를 통해 가져와 JSON 파일에 추가합니다.
+ */
+const continueWritingGameData = async () => {
+    const games = await useJSON.readJSON('games.json');
+    appIDs = games.map(game => game.appid);
+    appNames = games.map(game => game.name);
     appInfos = await useJSON.readJSON('gameData.json');
-
-    if (0 < appIDs.length) {
-        console.log(appIDs.length, 'missing data found.')
-        console.log('Continued from ID', appIDs[0]);
-        await getNextBatch(0).then(async () => { await useJSON.writeJSON(appInfos, 'gameData.json'); });
+    
+    const startIndex = appInfos.length;
+    if (startIndex < appIDs.length) {
+        console.log('Continue indexing from', startIndex);
+        await processNextBatch(startIndex).then(async () => { await useJSON.writeJSON(appInfos, 'gameData.json'); });
     } else {
         console.log('GameData Upload Completed!');
     }
 }
 
-/** 누락된 데이터 검사 */
-const omissionCheck = async () => {
+/**
+ * JSON 파일에서 게임 목록을 읽어와 `gameData.json` 파일에서 누락된 게임 ID를 찾아 출력 및 반환합니다.
+ *
+ * @returns {Promise<number[]>} - 누락된 게임 ID 목록
+ */
+const findOmission = async () => {
     const games = await useJSON.readJSON('games.json');
+    appIDs = games.map(game => game.appid);
     const gameData = await useJSON.readJSON('gameData.json');
+    const compareIDs = gameData.map(app => app.id);
 
     let omissionList = [];
-
-    games.map(app => {
-        if (!gameData.find(compare => {compare.id === app.appid}))
-            omissionList.push(app);
+    appIDs.map(appID => {
+        if (!compareIDs.find(compareID => {compareID === appID}))
+            omissionList.push(appID);
     });
 
+    console.log('omission List:\n', omissionList);
     return omissionList;
 }
 
-const oneAppCheck = async (id) => {
-    const browser = await chromium.launch({ headless: true });
-    const browserContext = await browser.newContext();
-    await browserContext.addCookies(
-        [
-            { name: 'lastagecheckage', value: '29-1-2000', url: 'https://store.steampowered.com' },
-            { name: 'birthtime', value: '946393201', url: 'https://store.steampowered.com' }
-        ]
-    );
-
-    let appInfo = {};
-
-    appInfo.id = id;
-    appInfo.name = appNames[appIDs.indexOf(id)];
-    const page = await browserContext.newPage();
-    try {
-        await page.goto(`https://store.steampowered.com/app/${id}`);
-        
-        if (page.url() !== 'https://store.steampowered.com/' && page.url() !== 'https://store.steampowered.com/app/' + id) {
-
-            // ======================================== Price ==========================================
-            const onSale = await page.$('.game_area_purchase_game_wrapper .discount_original_price');
-            const forSale = await page.$('.game_area_purchase_game_wrapper');
-
-            if (onSale !== null) { // 할인 중
-                const priceInfo = await page.$eval('.game_purchase_discount:nth-child(1)', el => el.textContent.trim());
-                const matchedPrice = /(-?\d+)%.*?([\d,]+).*?([\d,]+|$)/g.exec(priceInfo);
-                appInfo.is_discount = true;
-                appInfo.price = {
-                    discount_pct: matchedPrice[1],
-                    original_price: matchedPrice[2].replace(',', ''),
-                    discount_price: matchedPrice[3].replace(',', '')
-                }
-            } else if (forSale) { // 판매 중
-                let priceInfo = await page.$eval('.game_purchase_price:nth-child(1)', el => el.textContent.trim());
-                if (priceInfo === 'Download the Free Demo') {
-                    priceInfo = await page.$eval('.game_purchase_price:nth-child(2)', el => el.textContent.trim());
-                }
-                const matchedPrice = priceInfo.match(/\d+,\d+/);
-                appInfo.is_discount = false;
-                appInfo.price = {
-                    original_price: parseInt(matchedPrice[0].replace(/,/g, ""))
-                }
-            }
-            // =========================================================================================
-
-            // ====================================== Requirement ======================================
-            const requirementsInput = await page.$eval('.game_area_sys_req:nth-child(1)', el => el.textContent.trim());
-            console.log(requirementsInput);
-            appInfo.requirement = parseSystemRequirements(requirementsInput);
-            // =========================================================================================
-        }
-        console.log('Push Completed! app ID', id);
-    } catch (error) {
-        console.error(`Error occurred while processing app ID ${id}: ${error}`);
-    } finally {
-        console.log(appInfo);
-        await browser.close();
-    }
+/**
+ * 특정 게임의 상세 정보를 가져와 `fetchGameDetails()` 함수를 사용하여 처리한 후, 콘솔을 통해 출력합니다.
+ *
+ * @param {number} id - 게임의 ID
+ * @returns {Promise<void>}
+ */
+const checkSingleApp = async (id) => {
+    await fetchGameDetails(id);
+    console.log(appInfos);
 }
 
-/** Steam에서 서비스 중인 모든 게임 리스트를 가져옵니다. */
-async function getAppList() {
+/**
+ * 게임 목록을 가져와 JSON 파일에 저장하는 비동기 함수입니다.
+ *
+ * @returns {Promise<void>}
+ */
+async function fetchAppList() {
     return steam.getAppList().then(async apps => {
         await useJSON.writeJSON(apps, 'games.json');
     }).catch(error => {
@@ -120,170 +92,129 @@ async function getAppList() {
     });
 }
 
-/** 게임의 세부 데이터를 불러와 gameData.json에 저장합니다. !!호출 전 반드시 appIDs와 appNames를 초기화할 것!! */
-async function getNextBatch(startIndex) {
+/**
+ * 다음 일괄 처리를 수행하는 비동기 함수입니다.
+ * 시작 인덱스를 기준으로 일괄 처리할 게임 ID를 가져온 다음, API를 호출하여 게임 세부 정보를 추출합니다.
+ * 이후 다음 일괄 처리를 예약하고, 모든 호출이 완료되면 추출된 정보를 JSON 파일에 저장합니다.
+ * 
+ * !!호출하기 전 반드시 appIDs, appNames 변수를 초기화해주어야 합니다.
+ *
+ * @param {number} startIndex - 일괄 처리의 시작 인덱스
+ * @returns {Promise<void>}
+ */
+async function processNextBatch(startIndex) {
     const endIndex = Math.min(startIndex + batchSize, appIDs.length);
     const batchIDs = appIDs.slice(startIndex, endIndex);
-
-    // 페이지 크롤링
-    const browser = await chromium.launch({ headless: true });
-    const browserContext = await browser.newContext();
-    await browserContext.addCookies(
-        [
-            { name: 'lastagecheckage', value: '29-1-2000', url: 'https://store.steampowered.com' },
-            { name: 'birthtime', value: '946393201', url: 'https://store.steampowered.com' }
-        ]
-    );
-
-    const promises = batchIDs.map(async (id, index) => {
-        let appInfo = {};
-
-        // ID, NAME
-        appInfo.id = id;
-        appInfo.name = appNames[index];
-        
-        const page = await browserContext.newPage();
-        try {
-            await page.goto(`https://store.steampowered.com/app/${id}`);
-            
-            // 해당 게임의 스토어 페이지가 존재하지 않을 경우 세부 데이터 스크래핑 방지
-            if (page.url() !== 'https://store.steampowered.com/' && page.url() !== 'https://store.steampowered.com/app/' + id) {
-                
-                // Price
-                const isDiscount = await page.$('.game_area_purchase_game_wrapper .discount_original_price');
-                if (isDiscount !== null) {
-                    const priceInfo = await page.$eval('.game_purchase_discount:nth-child(1)', el => el.textContent.trim());
-                    const matchedPrice = /(-?\d+)%.*?([\d,]+).*?([\d,]+|$)/g.exec(priceInfo);
-                    appInfo.is_discount = true;
-                    appInfo.price = {
-                        discount_pct: matchedPrice[1],
-                        original_price: matchedPrice[2].replace(',', ''),
-                        discount_price: matchedPrice[3].replace(',', '')
-                    }
-                } else {
-                    let priceInfo = await page.$eval('.game_purchase_price:nth-child(1)', el => el.textContent.trim());
-                    if (priceInfo === 'Download the Free Demo') {
-                        priceInfo = await page.$eval('.game_purchase_price:nth-child(2)', el => el.textContent.trim());
-                    }
-                    const matchedPrice = priceInfo.match(/\d+,\d+/)[0];
-                    if(matchedPrice) {
-                        appInfo.is_discount = false;
-                        appInfo.price = {
-                            original_price: parseInt(matchedPrice[0].replace(/,/g, ""))
-                        }
-                    }
-                }
     
-                // Requirement
-
-                // 최소만 있을 때
-                
-                const minimum = await page.$eval('.game_area_sys_req_full:nth-child(1)', el => el.textContent.trim());
-                const minimumRequirementsObject = extractData(minimum);
-                
-                console.log(minimumRequirementsObject);
-
-                // 최소, 권장이 있을 때
-                // .game_area_sys_req_leftCol // 최소
-                // .game_area_sys_req_rightCol // 권장
-                
-                // const minimum = details.pc_requirements.minimum;
-                // const recommended = details.pc_requirements.recommended;
-                // const minimumRequirementsObject = extractData(minimum);
-                // const recommendedRequirementsObject = extractData(recommended);
-
-                //     price_overview: details.price_overview,
-                //     categories: details.categories,
-                //     genres: details.genres
-            }
-            console.log('Push Completed! app ID', id);
-        } catch (error) {
-            if (error.toString().includes('page.goto')) {
-                throw `Error occurred while processing app ID ${id}: ${error}`;
-            } else {
-                console.error(`Error occurred while processing app ID ${id}: ${error}`);
-            }
-        } finally {
-            appInfos.push(appInfo);
-            console.log(appInfo);
-            await page.close();
-        }
-    });
-
-    // 다음 호출을 예약
     if (endIndex < appIDs.length) {
-        setTimeout(() => getNextBatch(endIndex).then(async () => { await useJSON.writeJSON(appInfos, 'gameData.json'); }), interval);
+        setTimeout(() => processNextBatch(endIndex).then(async () => { await useJSON.writeJSON(appInfos, 'gameData.json'); }), interval);
     }
 
-    return Promise.all(promises).then(async () => { await browser.close(); });
+    for (const id of batchIDs) {
+        if (appInfos.find((app) => {app.id === id}))
+            throw `\n\n\n\n\n${id} is Duplicate value!!!!!\n\n\n\n\n`;
+
+        fetchGameDetails(id);
+    }
 };
 
 /**
- * 입력된 시스템 요구사항 문자열을 파싱하여 최소 요구사항과 권장 요구사항을 추출합니다.
- * @param {string} input - 시스템 요구사항 문자열
- * @returns {object} - 최소 요구사항과 권장 요구사항을 담은 객체
+ * 지정된 게임 ID에 대한 게임 세부 정보를 가져오는 비동기 함수입니다.
+ * Steam API를 사용하여 게임 세부 정보를 요청하고, 요구 사항과 다른 세부 정보를 추출하여 객체에 저장합니다.
+ * 추출된 정보는 appInfos 배열에 추가되며, 에러가 발생한 경우에도 해당 정보는 저장됩니다.
+ *
+ * @param {number} id - 가져올 게임의 고유 ID
+ * @returns {Promise<void>}
  */
-function parseSystemRequirements(input) {
-    const requirements = {
-      minimum: {},
-      recommended: {},
-    };
-  
-    const minimumRegex = /Minimum:([\s\S]*?)(?=Recommended:|$)/;
-    const recommendedRegex = /Recommended:([\s\S]*)/;
-  
-    const minimumMatch = input.match(minimumRegex);
-    if (minimumMatch) {
-      const minimumRequirements = minimumMatch[1].trim();
-      console.log(minimumRequirements);
-      requirements.minimum = extractSystemRequirements(minimumRequirements);
-    }
-  
-    const recommendedMatch = input.match(recommendedRegex);
-    if (recommendedMatch) {
-      const recommendedRequirements = recommendedMatch[1].trim();
-      console.log(recommendedRequirements);
-      requirements.recommended = extractSystemRequirements(recommendedRequirements);
-    }
-  
-    return requirements;
+async function fetchGameDetails(id) {
+    await steam.getGameDetails(id)
+        .then(details => {
+            const minimum = details.pc_requirements.minimum;
+            const recommended = details.pc_requirements.recommended;
+            const minimumRequirementsObject = extractRequirements(minimum);
+            const recommendedRequirementsObject = extractRequirements(recommended);
+            appInfos.push({
+                name: appNames[appIDs.indexOf(id)],
+                id: id,
+                is_free: details.is_free,
+                supported_languages: extractSupportedLanguages(details.supported_languages),
+                header_image: details.header_image,
+                requirements: {
+                    minimum: minimumRequirementsObject,
+                    recommended: recommendedRequirementsObject
+                },
+                price_overview: details.price_overview,
+                categories: details.categories,
+                genres: details.genres
+            });
+            console.log('Push Completed! app ID', id);
+        })
+        .catch(error => {
+            appInfos.push({
+                name: appNames[appIDs.indexOf(id)],
+                id: id,
+                requirements: {},
+            });
+            if (error.message === 'No app found') console.log(`Invalid app ID ${id}, skipping...`);
+            else console.log(error);
+        });
 }
 
 /**
- * 입력된 문자열에서 시스템 요구사항을 추출하는 함수입니다.
- * 문자열을 기준으로 각 요구사항을 `OS`, `Processor`, `Memory`, `Graphics`, `DirectX`, `Storage`로 분리합니다.
- * 빈 요구사항은 빈 문자열 또는 빈 배열로 표시됩니다.
- *
- * @param {string} input - 추출할 시스템 요구사항이 포함된 문자열
- * @returns {object} - 추출된 시스템 요구사항 객체
+ * 주어진 입력 문자열에서 영어 언어를 추출하여 배열 형태로 반환하는 함수입니다.
+ * 
+ * @param {string} input - 추출할 언어를 포함한 입력 문자열
+ * @returns {string[]} - 추출된 영어 언어 배열
  */
-function extractSystemRequirements(input) {
-    const requirements = {};
+function extractSupportedLanguages(input) {
+    const languageRegex = /([A-Za-z\s-]+)(?=<strong>\*<\/strong>?)/g;
+    const matches = input.match(languageRegex);
+    const languages = matches ? matches.map(match => match.trim()) : [];
+    return languages;
+}
 
-    const osMatch = input.match(/OS:\s(.*?)Processor:/);
-    requirements.OS = osMatch ? osMatch[1].trim() : '';
+/**
+ * 주어진 입력 문자열에서 시스템 요구 사항 데이터를 추출하여 객체 형태로 반환하는 함수입니다.
+ * 
+ * @param {string} input - 추출할 시스템 요구 사항을 포함한 입력 문자열
+ * @returns {Object} - 추출된 시스템 요구 사항 객체
+ */
+function extractRequirements(input) {
+    if (!input) return {};
 
-    const processorMatch = input.match(/Processor:\s(.*?)Memory:/);
-    requirements.Processor = processorMatch ? processorMatch[1].trim() : '';
+    const OSMatch = input.match(/<strong>OS:<\/strong>(.*?)<br>/);
+    const OS = OSMatch ? OSMatch[1].trim() : '';
 
-    const memoryMatch = input.match(/Memory:\s(.*?)Graphics:/);
-    requirements.Memory = memoryMatch ? memoryMatch[1].trim() : '';
+    const ProcessorMatch = input.match(/<strong>Processor:<\/strong>(.*?)<br>/);
+    const Processor = ProcessorMatch ? ProcessorMatch[1].trim() : '';
 
-    const graphicsMatch = input.match(/Graphics:\s(.*?)DirectX:/);
-    requirements.Graphics = graphicsMatch ? graphicsMatch[1].trim() : '';
+    const MemoryMatch = input.match(/<strong>Memory:<\/strong>(.*?)<br>/);
+    const Memory = MemoryMatch ? MemoryMatch[1].trim() : '';
 
-    const directXMatch = input.match(/DirectX:\s(.*?)Storage:/);
-    requirements.DirectX = directXMatch ? directXMatch[1].trim() : '';
+    const GraphicsMatch = input.match(/<strong>Graphics:<\/strong>(.*?)<br>/);
+    const Graphics = GraphicsMatch ? GraphicsMatch[1].trim() : '';
 
-    const storageMatch = input.match(/Storage:\s(.*?)$/);
-    requirements.Storage = storageMatch ? storageMatch[1].trim() : '';
+    const DirectXMatch = input.match(/<strong>DirectX:<\/strong>(.*?)<br>/);
+    const DirectX = DirectXMatch ? DirectXMatch[1].trim() : '';
 
-    return requirements;
+    const StorageMatch = input.match(/<strong>Storage:<\/strong>(.*?)<br>/);
+    const Storage = StorageMatch ? StorageMatch[1].trim() : '';
+
+    const RequirementsObject = {
+        OS: OS,
+        Processor: Processor.split(/(Intel|AMD)\s.*?(?=\s\()/),
+        Memory: Memory,
+        Graphics: Graphics.split(/,| or |\/|\s/),
+        DirectX: DirectX,
+        Storage: Storage
+    }
+
+    return RequirementsObject;
 }
 
 module.exports = {
-    updateGameData,
-    writeGameDataContinue,
-    omissionCheck,
-    oneAppCheck
+    updateGameDatabase,
+    continueWritingGameData,
+    findOmission,
+    checkSingleApp
 }
