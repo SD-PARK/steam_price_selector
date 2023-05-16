13518387ee9af5f45ccabb20e2e7860d37e0685c
SD-PARK <tkdeh129@naver.com>
2023-05-16 17:59:33 +0900

    test: game data fetch

--- src/routes/testAPI.js
+++ src/routes/testAPI.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const useJSON = require('../apps/useJSON');
 // require('../apps/benchmarkCrawler').cpuUpdate(); // CPU Data 갱신
 // require('../apps/benchmarkCrawler').gpuUpdate(); // GPU Data 갱신
-// require('../apps/steamData').checkSingleApp(10); // Steam Data 갱신 // Counter-Strike
-// require('../apps/steamData').checkSingleApp(271590); // Steam Data 갱신 // Grand Theft Auto 5
-// require('../apps/steamData').checkSingleApp(990080); // Steam Data 갱신 // Hogwarts Legacy
-// require('../apps/steamData').checkSingleApp(2369390); // Steam Data 갱신 // Farcry 6
-// require('../apps/steamData').checkSingleApp(1150530); // Steam Data 갱신 // Wizard with a Gun
-// require('../apps/steamData').checkSingleApp(1415280); // Steam Data 갱신 // Faerie Afterlight
-// require('../apps/steamData').findOmission();
+// require('../apps/gameDetailsFetcher').checkSingleApp(10); // Steam Data 갱신 // Counter-Strike
+// require('../apps/gameDetailsFetcher').checkSingleApp(271590); // Steam Data 갱신 // Grand Theft Auto 5
+// require('../apps/gameDetailsFetcher').checkSingleApp(990080); // Steam Data 갱신 // Hogwarts Legacy
+// require('../apps/gameDetailsFetcher').checkSingleApp(2369390); // Steam Data 갱신 // Farcry 6
+// require('../apps/gameDetailsFetcher').checkSingleApp(1150530); // Steam Data 갱신 // Wizard with a Gun
+// require('../apps/gameDetailsFetcher').checkSingleApp(1415280); // Steam Data 갱신 // Faerie Afterlight
+// require('../apps/gameDetailsFetcher').findOmission();
 require('../apps/gameDetailsFetcher').continueWritingGameData();
 
 router.get('/', async (req, res) => {
