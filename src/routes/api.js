561f1612f655febfc3dd4743caec913180346ccf
SD-PARK <tkdeh129@naver.com>
2023-05-30 14:25:03 +0900

    test: get route 제거

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const apiController = require('./_controller/apiController');
 
-router.get('/', (req, res) => {
-    res.send('good');
-});
 router.post('/games', apiController.validateInputMiddleware, apiController.postGames);
 
 module.exports = router;
